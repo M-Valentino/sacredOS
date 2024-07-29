71deaeecfa6285d64937dce0de3077df2d2bb1a2
Mark Valentino <mike9377mike9377@gmail.com>
2024-07-28 23:17:15 -0400

    install button working

---
+++ public/appStore/apps/peterAlert/install.js
@@ -0,0 +1,18 @@
+window.top.postMessage(`MK:F[programs/peterAlert.html]`, '*');
+
+
+async function copyData() {
+  try {
+    const response = await fetch("/appStore/apps/PeterAlert/peterAlert.html");
+    if (response.ok) {
+      const content = await response.text();
+      window.top.postMessage(`SF:[programs/peterAlert.html]${content}`, "*");
+    } else {
+      console.error(`Error fetching file HTTP status ${response.status}`);
+    }
+  } catch (err) {
+    console.error(`Error fetching file : ${err}`);
+  }
+}
+
+copyData();
--- public/appStore/apps/test/install.js
+++
@@ -1,2 +0,0 @@
-window.top.postMessage(`MK:F[programs/test.html]`, '*');
-window.top.postMessage(`SF:[programs/test.html]<html>alert("t");</html>`, "*");
