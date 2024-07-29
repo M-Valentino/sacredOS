f8859cf06321b3589afdcc6c9082e1d93231a6cb
Mark Valentino <mike9377mike9377@gmail.com>
2024-07-29 01:38:01 -0400

    added alert

--- public/appStore/apps/peterAlert/install.js
+++ public/appStore/apps/peterAlert/install.js
@@ -1,19 +1,25 @@
-window.top.postMessage(`MK:F[programs/peterAlert.html]`, '*');
-
-
 async function copyData() {
   try {
     const response = await fetch("/appStore/apps/PeterAlert/peterAlert.html");
     if (response.ok) {
       const content = await response.text();
       window.top.postMessage(`SF:[programs/peterAlert.html]${content}`, "*");
+      return true;
     } else {
       console.error(`Error fetching file HTTP status ${response.status}`);
     }
   } catch (err) {
     console.error(`Error fetching file : ${err}`);
   }
+  return false;
 }
 
-copyData();
-window.top.postMessage("POPULATE-MENU");
+window.top.postMessage(`MK:F[programs/peterAlert.html]`, "*");
+if (copyData()) {
+  window.top.postMessage("POPULATE-MENU");
+  alert(
+    'Peter Alert installed! Go to your programs folder. I\'m working on making installed programs show up in the "start" menu.'
+  );
+} else {
+  alert("Could Not install Peter Alert.");
+}
