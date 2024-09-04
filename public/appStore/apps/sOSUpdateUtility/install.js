a88c416c557a445f13d6196c36fc6c01f04731a7
Mark Valentino <mike9377mike9377@gmail.com>
2024-09-04 13:12:54 -0400

    updated versions and changlog

--- public/appStore/apps/sOSUpdateUtility/install.js
+++ public/appStore/apps/sOSUpdateUtility/install.js
@@ -1,4 +1,4 @@
-const appName = "osUpdate1.4.html";
+const appName = "osUpdate1.5.html";
 async function copyData() {
   try {
     const response = await fetch(`/appStore/apps/sOSUpdateUtility/${appName}`);
--- public/system/kernel.js
+++ public/system/kernel.js
@@ -285,7 +285,7 @@ window.onmessage = function (e) {
       }
       return;
     } else if (e.data == "REQ:OSV") {
-      e.source.postMessage("OSV:1.4", "*");
+      e.source.postMessage("OSV:1.5", "*");
     } else if (e.data.startsWith("SF:[")) {
       const rightBracketIndex = e.data.indexOf("]");
 
