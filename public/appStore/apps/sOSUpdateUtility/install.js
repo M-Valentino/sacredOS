67a6d8697a07b3d38e76b8bb34b8f614dc2f1f53
Mark Valentino <mike9377mike9377@gmail.com>
2024-11-27 23:07:30 -0500

    updated versioning

--- public/appStore/apps/sOSUpdateUtility/install.js
+++ public/appStore/apps/sOSUpdateUtility/install.js
@@ -1,4 +1,4 @@
-const appName = "osUpdate1.13.html";
+const appName = "osUpdate1.14.html";
 async function copyData() {
   try {
     const response = await fetch(`/appStore/apps/sOSUpdateUtility/${appName}`);
